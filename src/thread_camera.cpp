50eda3f204aae778b1855cf10ef2af5d5f0d4ca7
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2021-12-30 16:10:08 +0100

    apply camera options to capture only when using a camera

--- src/thread_camera.cpp
+++ src/thread_camera.cpp
@@ -8,11 +8,14 @@ void thread_camera(const Options& options, Shared* shared)
 {
     cv::VideoCapture capture = options.filename.empty() ? cv::VideoCapture(options.cameraIndex) : cv::VideoCapture(options.filename);
     
-    printf("I: cv::CAP_PROP_FRAME_WIDTH = %d\n", options.camera_width);
-    capture.set(cv::CAP_PROP_FRAME_WIDTH,  (double)options.camera_width);
-    //capture.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
-    capture.set(cv::CAP_PROP_FPS,           (double)options.camera_fps);
-    capture.set(cv::CAP_PROP_BUFFERSIZE, 1);
+    if ( options.filename.empty() ) 
+    {
+        printf("I: set cv::CAP_PROP_FRAME_WIDTH = %d\n", options.camera_width);
+        capture.set(cv::CAP_PROP_FRAME_WIDTH,  (double)options.camera_width);
+        //capture.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+        capture.set(cv::CAP_PROP_FPS,           (double)options.camera_fps);
+        capture.set(cv::CAP_PROP_BUFFERSIZE, 1);
+    }
 
     int CurrNr = 0;
 
