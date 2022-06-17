315a59eedab14f5477b2dfe9fc7d6dcfe3f711ee
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2022-06-17 14:37:06 +0200

    better code structure

--- src/thread_camera.cpp
+++ src/thread_camera.cpp
@@ -4,6 +4,23 @@
 #include "shared.h"
 #include "stats.h"
 
+const cv::Scalar RED   = cv::Scalar(0,0,255);
+const cv::Scalar BLUE  = cv::Scalar(255,0,0);
+const cv::Scalar GREEN = cv::Scalar(0,255,0);
+
+void copy_status_frame_to(cv::OutputArray dest, const char* text, const cv::Scalar& color, const int retryCount) {
+
+    cv::String msg(text);
+
+    if (retryCount >= 0) {
+        msg += " #" + std::to_string(retryCount);
+    }
+
+    cv::Mat status( cv::Mat::zeros(480, 640, 16) );
+    cv::putText(status, msg, cv::Point(10, 240), cv::FONT_HERSHEY_SIMPLEX, 1, color, 2);
+    status.copyTo(dest);
+}
+
 bool try_open_capture(cv::VideoCapture* capture, const Options& options, Shared* shared)
 {
     if ( options.filename.empty() ) {
@@ -28,11 +45,8 @@ void thread_camera(const Options& options, Shared* shared)
     
     int CurrNr = 0;
 
-    {
-        cv::Mat status( cv::Mat::zeros(480, 640, 16) );
-        cv::putText(status, "starting up...", cv::Point(10, 240), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0,255,0), 2);
-        status.copyTo(shared->frame_buf[CurrNr]);
-    }
+    copy_status_frame_to(shared->frame_buf[CurrNr], "starting up...", GREEN, -1);
+    shared->detectSettings.detecting.store(false);
     
     shared->frame_buf_slot.store(CurrNr);
     int WorkNr = CurrNr;
@@ -40,6 +54,7 @@ void thread_camera(const Options& options, Shared* shared)
 
     int delay_for_realtime_video_millis = 0;
     bool should_set_cols_and_rows = true;
+    int retryCount = 0;
 
     for (;;)
     {
@@ -52,46 +67,44 @@ void thread_camera(const Options& options, Shared* shared)
         {
             if ( ! try_open_capture(&capture, options, shared) ) {
                 fprintf(stderr,"E: cannot open capture device. retry...\n");
-
-                cv::Mat status( cv::Mat::zeros(480, 640, 16) );
-                cv::putText(status, "could not open camera", cv::Point(10, 240), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0,0,255), 2);
-                status.copyTo(shared->frame_buf[CurrNr]);
-                shared->detectSettings.detecting.store(false);
+                copy_status_frame_to(shared->frame_buf[CurrNr], "could not open camera", RED, retryCount);
+                retryCount += 1;
             }
+            else
+            {
+                printf("I: capture device opened successfully\n");
+                copy_status_frame_to(shared->frame_buf[CurrNr], "camera opened", GREEN, -1);
+            }
+            shared->detectSettings.detecting.store(false);
             should_set_cols_and_rows = true;
         }
-        
-        if ( capture.isOpened() ) {
-            if ( ! capture.read( shared->frame_buf[CurrNr] ) )
-            {
-                fprintf(stderr, "E: capture.read()\n");
-                capture.release();
-                cv::Mat status( cv::Mat::zeros(480, 640, 16) );
-                cv::putText(status, "could not read from camera", cv::Point(10, 240), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(0,0,255), 2);
-                status.copyTo(shared->frame_buf[CurrNr]);
-                should_set_cols_and_rows = true;
-                shared->detectSettings.detecting.store(false);
-            }
-            else {
-                if ( should_set_cols_and_rows ) {
-                    shared->detectSettings.set_frame( shared->frame_buf[CurrNr].cols
-                                                    , shared->frame_buf[CurrNr].rows );
-                    delay_for_realtime_video_millis = options.filename.empty() ? 0 : 1000 / ((int)capture.get(cv::CAP_PROP_FPS)) * options.video_playback_slowdown_factor;
-                    should_set_cols_and_rows = false;
-                    printf("I: COLS and ROWS were set to: %dx%d\n", shared->frame_buf[CurrNr].cols, shared->frame_buf[CurrNr].rows);
-                }
-                shared->detectSettings.detecting.store(true);
-            }
-        }
+        else if ( ! capture.read( shared->frame_buf[CurrNr] ) )
         {
-            int PrevNr = std::atomic_exchange( &(shared->frame_buf_slot), CurrNr );
-            int NextNr = ( PrevNr == -1 ) ? 3 - ( WorkNr + CurrNr ) : PrevNr;
-                WorkNr = CurrNr;
-                CurrNr = NextNr;
-
-            shared->camera_frame_ready.notify_one();
-            shared->stats.camera_frames++;
+            fprintf(stderr, "E: capture.read()\n");
+            capture.release();
+            copy_status_frame_to(shared->frame_buf[CurrNr], "could not read from camera", RED, retryCount);
+            should_set_cols_and_rows = true;
+            shared->detectSettings.detecting.store(false);
+            retryCount += 1;
         }
+        else if ( should_set_cols_and_rows ) {
+            should_set_cols_and_rows = false;
+            retryCount = 0;
+
+            delay_for_realtime_video_millis = options.filename.empty() ? 0 : 1000 / ((int)capture.get(cv::CAP_PROP_FPS)) * options.video_playback_slowdown_factor;
+            shared->detectSettings.set_frame( shared->frame_buf[CurrNr].cols
+                                            , shared->frame_buf[CurrNr].rows );
+            shared->detectSettings.detecting.store(true);
+            printf("I: COLS and ROWS were set to: %dx%d\n", shared->frame_buf[CurrNr].cols, shared->frame_buf[CurrNr].rows);
+        }
+
+        int PrevNr = std::atomic_exchange( &(shared->frame_buf_slot), CurrNr );
+        int NextNr = ( PrevNr == -1 ) ? 3 - ( WorkNr + CurrNr ) : PrevNr;
+            WorkNr = CurrNr;
+            CurrNr = NextNr;
+
+        shared->camera_frame_ready.notify_one();
+        shared->stats.camera_frames++;
 
         if ( !capture.isOpened() ) {
            std::this_thread::sleep_for( std::chrono::milliseconds(2000) ); 
