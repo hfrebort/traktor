4ae80dc9888148ef60b6b011770781c5e2b79b41
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2023-06-17 09:47:48 +0200

    split CMakeFiles for src and test. but unified for "one cmake project"

--- test/traktor_test.cpp
+++ test/traktor_test.cpp
@@ -1,10 +1,21 @@
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
 
-TEST(TraktorTests, TestIntegerOne_One2)
+#include "../src/shared.h"
+
+bool find_point_on_nearest_refline(
+      const cv::Point&        plant
+    , const ReflinesSettings& settings
+    , float  *nearest_refLine_x
+    , float  *deltaPx
+    , float  *refLines_distance_px);
+
+TEST(TraktorTests, Test_plant_on_middle_line)
 {
-    const auto expected = 1;
-    const auto actual = 1 * 1;
-    ASSERT_EQ(expected, actual);
+    auto plant = cv::Point(0,0);
+    ReflinesSettings refline;
+    refline.y_fluchtpunkt = 0;
+
+    ASSERT_EQ(0, plant.x + plant.y);
 }
 
 TEST(TraktorTests, TestIntegerOne_One)
